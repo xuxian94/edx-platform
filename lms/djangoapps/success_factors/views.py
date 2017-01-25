9a1627c44582f8db04cd5b01dd07e673140d878d
Brittney Exline <bexline@edx.org>
2017-01-25 17:13:00 -0500

    first quick and diry integration with the apiary api and reading from the config models

--- lms/djangoapps/success_factors/views.py
+++ lms/djangoapps/success_factors/views.py
@@ -1,8 +1,10 @@
+import requests
+import time
 from rest_framework import status
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from success_factors.models import CompletionStatusEvent
+from success_factors.models import CompletionStatusEvent, OCNWebServicesConfig, OCNEnterpriseCustomerConfig
 from success_factors.serializers import CompletionStatusEventSerializer
 
 # Create your views here.
@@ -17,6 +19,20 @@ class SuccessFactorsList(APIView):
     def post(self, request, format=None):
         serializer = CompletionStatusEventSerializer(data=request.data)
         if serializer.is_valid():
+            ocn_web_services_config = OCNWebServicesConfig.objects.get(pk=1)
+            ocn_enterprise_customer_config = OCNEnterpriseCustomerConfig.objects.get(pk=1)
+            sf_url = ocn_enterprise_customer_config.sf_base_url + '/' + ocn_web_services_config.completion_status_api_path
+            sf_payload = {
+                'userID': serializer.data['sf_user_id'],
+                'courseID': serializer.data['course_id'],
+                'providerID': 'EDX',
+                'courseCompleted': True,
+                'completedTimestamp': int(time.time()),
+                'instructorName': serializer.data['instructor_name'],
+                'grade': serializer.data['grade']
+            }
+
+            response = requests.post(sf_url, data=sf_payload)
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -34,18 +50,6 @@ class SuccessFactorsDetail(APIView):
         serializer = CompletionStatusEventSerializer(completion_status)
         return Response(serializer.data)
 
-    def put(self, request, pk, format=None):
-        completion_status = self.get_object(pk)
-        serializer = CompletionStatusEventSerializer(completion_status, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk, format=None):
-        completion_status = self.get_object(pk)
-        completion_status.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
